d2fe65bd33d1290e057c0b57e014fefaaacb33d5
gimhema <49871739+gimhema@users.noreply.github.com>
2023-10-29 00:36:27 +0900

    .
    .

---
+++ CSGameServerPractice/CSGameServerPractice/Server/Server.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace CSGameServerPractice.Server
+{
+    internal class Server
+    {
+        static Dictionary<Socket, byte[]> clients = new Dictionary<Socket, byte[]>();
+
+        static void Main(string[] args)
+        {
+            // 서버 소켓 생성 및 바인딩
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 12345);
+            Socket serverSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(endPoint);
+            serverSocket.Listen(10);
+
+            Console.WriteLine("Start Server . . . Wait for Client . . .");
+
+            // 클라이언트 접속 대기 스레드 시작
+            Thread listenThread = new Thread(ListenForClients);
+            listenThread.Start(serverSocket);
+
+            Console.ReadLine();
+        }
+
+        static void ListenForClients(object serverSocket)
+        {
+            Socket listener = (Socket)serverSocket;
+
+            while (true)
+            {
+                // 클라이언트 연결 대기
+                Socket clientSocket = listener.Accept();
+                Console.WriteLine("Connect Client . . . " + clientSocket.RemoteEndPoint);
+
+                // 클라이언트 관리
+                clients.Add(clientSocket, new byte[1024]);
+                Thread clientThread = new Thread(HandleClient);
+                clientThread.Start(clientSocket);
+            }
+        }
+
+        static void HandleClient(object clientSocket)
+        {
+            Socket client = (Socket)clientSocket;
+            byte[] buffer = clients[client];
+            while (true)
+            {
+                try
+                {
+                    // 클라이언트로부터 메시지 수신
+                    int bytesRead = client.Receive(buffer);
+                    if (bytesRead > 0)
+                    {
+                        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        Console.WriteLine("Client (" + client.RemoteEndPoint + "): " + message);
+
+                        // 모든 클라이언트에게 메시지 전송
+                        byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
+                        foreach (var otherClient in clients.Keys)
+                        {
+                            if (otherClient != client)
+                            {
+                                otherClient.Send(messageBuffer);
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    // 클라이언트 연결 끊김
+                    Console.WriteLine("Disconnect from (" + client.RemoteEndPoint + ").");
+                    clients.Remove(client);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ CSGameServerPractice/TestClientConsole/Client/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TestClientConsole.Client
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write Your Name: ");
+            string nickname = Console.ReadLine();
+
+            TcpClient client = new TcpClient();
+            client.Connect(IPAddress.Loopback, 12345);
+
+            Console.WriteLine("Connect Server.");
+
+            // 서버로부터 메시지를 수신하는 스레드 시작
+            Thread receiveThread = new Thread(() => ReceiveMessages(client));
+            receiveThread.Start();
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                SendMessage(client, nickname + ": " + message);
+            }
+        }
+
+        static void ReceiveMessages(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine(message);
+            }
+        }
+
+        static void SendMessage(TcpClient client, string message)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
+            stream.Write(messageBuffer, 0, messageBuffer.Length);
+        }
+    }
+}
