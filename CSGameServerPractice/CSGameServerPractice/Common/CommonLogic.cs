ac3511ad5d0736fea847745bafe657a3cc91a83b
gimhema <49871739+gimhema@users.noreply.github.com>
2023-11-21 00:47:06 +0900

    create class about game logic
    .

---
+++ CSGameServerPractice/CSGameServerPractice/Common/CommonLogic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLogic
+{
+    public class Vector
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public Vector(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    // 쿼터니언은 다루지않음
+    public class Rotation
+    {
+        public int roll;
+        public int pitch;
+        public int yaw;
+
+        public Rotation(int roll, int pitch, int yaw)
+        {
+            this.roll = roll;
+            this.pitch = pitch;
+            this.yaw = yaw;
+        }
+    }
+
+    public class Location
+    {
+        public Vector position;
+        public Rotation rotation;
+
+        public Location(Vector position, Rotation rotation)
+        {
+            this.position = position;
+            this.rotation = rotation;
+        }
+
+        public Vector GetLocation()
+        {
+            return this.position;
+        }
+
+        public Rotation GetRotation()
+        {
+            return this.rotation;
+        }
+    }
+
+}
---
+++ CSGameServerPractice/CSGameServerPractice/Server/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLogic;
+
+namespace CSGameServerPractice
+{
+    public class Player
+    {
+        private int uid;
+        private string name;
+        private Location playerLocation;
+        
+        public Player(int uid, string name)
+        {
+            this.uid = uid;
+            this.name = name;
+        }
+
+    }
+}
