610511a53c03c05461c989a2b677e96da6e8212b
gimhema <49871739+gimhema@users.noreply.github.com>
2023-12-23 22:58:04 +0900

    Update ConnectionHandler.cs
    add some methods

--- CSGameServerPractice/CSGameServerPractice/Server/ConnectionHandler.cs
+++ CSGameServerPractice/CSGameServerPractice/Server/ConnectionHandler.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ConnetionHandler
 {
     public class ConnectionHandler
     {
+        private static List<Socket> connections = new List<Socket>();
+        public ConnectionHandler() { }  
+
+        public void AddNewConnetion(Socket connetion)
+        {
+            connections.Add(connetion);
+        }
+
+        public void RemoveConnectionByID(int id)
+        {
+            if(connections.Count == 0)
+                return;
+            
+            if (id < 0)
+                return;
+
+            connections[id].Dispose();
+            connections.RemoveAt(id);
+        }
+
+        public void ConnectionsReset()
+        {
+            foreach (Socket connetion in connections)
+            {
+                connetion.Shutdown(SocketShutdown.Both);
+                connetion.Close();
+            }
+            connections.Clear(); 
+        }
+
     }
 }
